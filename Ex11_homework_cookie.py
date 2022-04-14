31caa9452a081453a02b50edf0fe638637b9b2c8
Ginger999 <sansara_@bk.ru>
2022-04-15 02:36:11 +0300

    expected_cookie_name added

--- Ex11_homework_cookie.py
+++ Ex11_homework_cookie.py
@@ -8,7 +8,8 @@ class TestCookie:
 
         # Ожидаемые значения запроса
         expected_status_code = 200
-        expected_cookie_MyCookie = "12345"
+        expected_cookie_name = "MyCookie"
+        expected_cookie_value = "12345"
 
         # Отправляем запрос
         response = requests.get(URL)
@@ -16,10 +17,12 @@ class TestCookie:
         # Получаем актуальные значения запроса
         actual_response_code = response.status_code
 
-        actual_cookies = dict(response.cookies)
-        # print(f"{actual_cookies}")
-        actual_cookie_MyCookie = actual_cookies.get("MyCookie")
-
         # Сравниваем актуальные и ожидаемые значения запроса
         assert actual_response_code == expected_status_code, f"Wrong response code {actual_response_code}"
-        assert actual_cookie_MyCookie == expected_cookie_MyCookie, f"Wrong cookie MyCookie value {actual_cookie_MyCookie}"
+
+        actual_cookies = dict(response.cookies)
+        # print(f"{actual_cookies}")
+        assert expected_cookie_name in actual_cookies, f"There is no field {expected_cookie_name} in cookies"
+
+        actual_cookie_value = actual_cookies[expected_cookie_name]
+        assert actual_cookie_value == expected_cookie_value, f"Wrong cookie MyCookie value {actual_cookie_value}"
