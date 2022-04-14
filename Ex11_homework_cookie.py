8565e4a19ffc7ceb8c82291bd1bfca5b879d28d7
Ginger999 <sansara_@bk.ru>
2022-04-15 02:16:29 +0300

    Ex11: Тест запроса на метод cookie

---
+++ Ex11_homework_cookie.py
@@ -0,0 +1,26 @@
+import json
+import requests
+
+
+class TestCookie:
+
+    def test_homework_cookie(self):
+        URL = "https://playground.learnqa.ru/api/get_cookie"
+
+        # Ожидаемые значения запроса
+        expected_status_code = 200
+        expected_cookie_MyCookie = "123451"
+
+        # Отправляем запрос
+        response = requests.get(URL)
+
+        # Получаем актуальные значения запроса
+        actual_response_code = response.status_code
+
+        actual_cookies = dict(response.cookies)
+        print(f"{actual_cookies}")
+        actual_cookie_MyCookie = actual_cookies.get("MyCookie")
+
+        # Сравниваем актуальные и ожидаемые значения запроса
+        assert actual_response_code == expected_status_code, f"Wrong response code {actual_response_code}"
+        assert actual_cookie_MyCookie == expected_cookie_MyCookie, f"Wrong cookie MyCookie value {actual_cookie_MyCookie}"
