9de1ae2719b805172d536788aaa4fc4db1ac7d51
Ginger999 <sansara_@bk.ru>
2022-04-25 20:45:41 +0300

    fixed: final url

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import requests
 
 response = requests.get("https://playground.learnqa.ru/api/long_redirect")
 i = 0
+url = ''
 for h in response.history:
     i += 1
-    print(f"{i}: {h}")
+    url = h.url
+    print(f"{i}: {h} {h.url}")
 
-print(f" \nNumber of redirects: {i} \nURL: {h}")
+print(f" \nNumber of redirects: {i} \nFinal URL: {url}")
